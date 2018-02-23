1587d34bafaf5237f3cb8724ba1d3183c73d3989
DESKTOP-TI81K93\gavin <gavinrob92@gmail.com>
2018-02-23 13:21:56 -0700

    initial login commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {AngularFireModule} from 'angularfire2';
+import { Routes, RouterModule } from '@angular/router';
 import {AngularFireDatabaseModule} from 'angularfire2/database';
 
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
+import { LoginComponent } from './login/login.component';
+import { CreateComponent } from './create/create.component';
 
+const appRoutes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'create', component: CreateComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
