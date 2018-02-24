4af7b787895eca0fb7108ff5d4c8ebdb3baa0283
garretthoffa <hoffa@cs.byu.edu>
2018-02-24 18:55:39 +0000

    started API routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var admin = require('firebase-admin');
+var serviceAccount = require('budgetappAdminKey.json');
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+var db = admin.firestore();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/createUser', function(req,res,next) {
+  admin.auth().createUser({
+    email: req.body.email,
+    password: req.body.password
+  })
+    .then(function(userRecord) {
+      console.log("Successfully created new user:", userRecord.uid);
+    })
+    .catch(function(error) {
+      console.log("error creating new user:", error);
+    })
+});
+
 
 module.exports = router;
