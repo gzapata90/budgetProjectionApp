2f7c6e7435ed67ae4a6f5781db5b04d768f19089
garretthoffa <hoffa@cs.byu.edu>
2018-02-28 20:41:17 +0000

    Added documentation for the different routes in the getRoutes.js file

--- backend/routes/getRoutes.js
+++ backend/routes/getRoutes.js
@@ -39,6 +39,10 @@ router.get('/', function(req,res,next) {
   res.status(200).send('<html><body><h1>Hello</h1></body></html>');
 });
 
+//This route expects the caller to pass in the budgetID of the budget 
+//that they would like for us to retrieve
+//it then takes that ID and acceses the database and returns the data on that 
+//budget as a map
 router.get('/budgetID', function(req,res,next) {
   var budgetRef = db.collection('budgets').doc(req.body.budgetID);
   
@@ -57,14 +61,24 @@ router.get('/budgetID', function(req,res,next) {
  })
 });
 
+//This route expects the caller to pass in a budgetID and a transactionID
+//these are the IDs of a specific transaction in a specific budget that
+//they would like to be retrieved
+//The transaction data will be returned as a map
 router.get('/budgetID/transactionID', function(req,res,next) {
 
 });
 
+//This route expects the caller to pass in a budgetID of the budget they
+//would like to retrieve all of the transactions for
+//The list of transactions will be returned as an array of transaction objects in a map
 router.get('/budgetID/transactions', function(req,res,next) {
-
+	var transactionsRef =db.collection('budgets').doc(req.body.budgetID).collection('transactions');
 });
 
+//This route expects the caller to pass in an accountID and a budgetID which
+//it will thean use to access the specified account from the specified budget
+//and return the information for that specific account as a map
 router.get('/budgetID/accountID', function(req, res, next) {
 	var accountRef=db.collection('budgets').doc(req.body.budgetID).collection('accounts').doc(req.body.accountID);
 	
