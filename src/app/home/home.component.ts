9c0abd269def49b2f7331482695322f51400dff9
dylanmoh <dylanmoh94@gmail.com>
2018-03-02 12:09:14 -0600

    home page part 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,9 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
   userName: string;
+  budget: [];
+  account: [];
+  transaction: [];
   private sub: any;
 
   constructor(private route: ActivatedRoute) {}
@@ -15,7 +18,19 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
       this.userName = params['user']; // (+) converts string 'id' to a number
-
+      
+      // In a real app: dispatch action to load the details here.
+      this.budget = [];
+      this.budget.budgetId = "budgetid";
+      this.budget.createDate = "02/28/2018";
+      this.budget.description = "budget description";
+      this.budget.goal = 1000;
+      this.budget.name = "My Budget";
+      this.budget.uidOfOwner = "abcid";
+      this.account = [];
+      this.account.balance = 500;
+      this.account.accountId = "accountId";
+      this.account.description = "account description";
       // In a real app: dispatch action to load the details here.
    });
   }
